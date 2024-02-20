e329d974f0bc9bd95d88e358508bee4c694daa1b
da_nielh <daniel37922434@gmail.com>
2024-02-21 02:18:07 +0300

    linking the server link on render site

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:5001/clone-app-acf89/us-central1/api'
+    // Local instance of firebase function 
+    // baseURL: 'http://127.0.0.1:5001/clone-app-acf89/us-central1/api'
+
+    // deployed version of firebase function
+    // baseURL: 'https://amazon-api-deploy-0nbu.onrender.com/'
+
+    // deployed version of amazon server on Render.com 
+    baseURL: 'https://amazon-api-deploy-0nbu.onrender.com/'
 })
 
 export {axiosInstance}
